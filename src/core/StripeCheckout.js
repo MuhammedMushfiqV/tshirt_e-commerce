e0eec7f4ccbccd42e770aba342d34035bd89d163
MuhammedMushfiqV <mushfiqmuhd707@gmail.com>
2020-08-04 09:56:44 +0530

    initial commit

---
+++ src/core/StripeCheckout.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import { isAutheticated } from "../auth/helper";
+import { cartEmpty, loadCart } from "./helper/cartHelper";
+import { Link } from "react-router-dom";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
+
+const StripeCheckout = ({
+  products,
+  setReload = f => f,
+  reload = undefined
+}) => {
+  const [data, setData] = useState({
+    loading: false,
+    success: false,
+    error: "",
+    address: ""
+  });
+
+  const token = isAutheticated() && isAutheticated().token;
+  const userId = isAutheticated() && isAutheticated().user._id;
+
+  const getFinalAmount = () => {
+    let amount = 0;
+    products.map(p => {
+      amount = amount + p.price;
+    });
+    return amount;
+  };
+
+  const makePayment = token => {
+    const body = {
+      token,
+      products
+    };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        console.log(response);
+        //call further methods
+        const orderData = {
+            products: products,
+            transaction_id: response.transaction.id,
+            amount: response.transaction.amount
+          };
+        createOrder(userId, token, orderData);
+
+        cartEmpty(() => {
+            console.log("Did we got a crash?");
+          });
+
+        setReload(!reload);
+
+      })
+      .catch(error => console.log(error));
+  };
+
+  const showStripeButton = () => {
+    return isAutheticated() ? (
+      <StripeCheckoutButton
+        stripeKey="pk_test_51H6AVCLfxSnrL19XQvD1PXrta00xq7JZPCyn6oPjioRLCm6L4EmArBNd36nE5DxvcWJHDiHThTx3XQabISsWlG6200mjCqR9io"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Buy Tshirts"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with stripe</button>
+      </StripeCheckoutButton>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-warning">Signin</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h3 className="text-white">Stripe Checkout {getFinalAmount()}</h3>
+      {showStripeButton()}
+    </div>
+  );
+};
+
+export default StripeCheckout;
